c954e310cce376edb5f920a1e5d758bc5f6514a8
saada <saadahmedscse@gmail.com>
2023-01-26 18:28:51 +0600

    Tested custom textview

--- app/src/main/java/com/saadahmedsoft/helperwidget/MainActivity.java
+++ app/src/main/java/com/saadahmedsoft/helperwidget/MainActivity.java
@@ -3,6 +3,10 @@ package com.saadahmedsoft.helperwidget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.saadahmedev.helperwidget.widgets.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.tv_custom);
+
+        tv.setOnClickListener(v -> {
+            //Toast.makeText(this, tv.getString(), Toast.LENGTH_SHORT).show();
+        });
+
+        tv.enable();
     }
 }
