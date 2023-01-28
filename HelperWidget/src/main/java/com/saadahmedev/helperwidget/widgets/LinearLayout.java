ed5bf30d16e6d6673d28bf58390e6539b2c78a9f
Saad <saadahmedscse@gmail.com>
2023-01-28 22:27:50 +0600

    Created Linear Layout

---
+++ HelperWidget/src/main/java/com/saadahmedev/helperwidget/widgets/LinearLayout.java
@@ -0,0 +1,102 @@
+package com.saadahmedev.helperwidget.widgets;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.GradientDrawable;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.saadahmedev.helperwidget.Helper;
+import com.saadahmedev.helperwidget.R;
+import com.saadahmedev.helperwidget.interfaces.OnLongPressListener;
+
+public class LinearLayout extends android.widget.LinearLayout {
+    public LinearLayout(Context context) {
+        super(context);
+
+        init(context, null, 0);
+    }
+
+    public LinearLayout(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        init(context, attrs, 0);
+    }
+
+    public LinearLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        init(context, attrs, defStyleAttr);
+    }
+
+    @NonNull
+    private static int[] cornerRadiusIds() {
+        return new int[]{
+                R.styleable.LinearLayout_cornerRadius,
+                R.styleable.LinearLayout_topLeftCornerRadius,
+                R.styleable.LinearLayout_topRightCornerRadius,
+                R.styleable.LinearLayout_bottomRightCornerRadius,
+                R.styleable.LinearLayout_bottomLeftCornerRadius,
+        };
+    }
+
+    @NonNull
+    private static int[] colorIds() {
+        return new int[]{
+                R.styleable.LinearLayout_backgroundColor,
+                R.styleable.LinearLayout_rippleColor
+        };
+    }
+
+    @NonNull
+    private static int[] strokeIds() {
+        return new int[]{
+                R.styleable.LinearLayout_strokeWidth,
+                R.styleable.LinearLayout_strokeColor
+        };
+    }
+
+    private void init(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.LinearLayout, defStyleAttr, 0);
+        Helper.initView(this, context, typedArray, new GradientDrawable());
+
+        Helper.initDefaultColors(colorIds());
+        Helper.initDefaultCornerRadius(cornerRadiusIds());
+        Helper.initDefaultClickable(R.styleable.LinearLayout_clickable);
+        Helper.initDefaultShape(R.styleable.LinearLayout_shape);
+        Helper.initDefaultStroke(strokeIds());
+
+        Helper.completeView();
+        typedArray.recycle();
+    }
+
+    public void enable() {
+        Helper.enable();
+    }
+
+    public void disable() {
+        Helper.disable();
+    }
+
+    public void visible() {
+        Helper.visible();
+    }
+
+    public void invisible() {
+        Helper.invisible();
+    }
+
+    public void gone() {
+        Helper.gone();
+    }
+
+    public void onClicked(com.saadahmedev.helperwidget.interfaces.OnClickListener listener) {
+        Helper.onClicked(listener);
+    }
+
+    public void onLongPressed(OnLongPressListener listener) {
+        Helper.onLongPressed(listener);
+    }
+}
