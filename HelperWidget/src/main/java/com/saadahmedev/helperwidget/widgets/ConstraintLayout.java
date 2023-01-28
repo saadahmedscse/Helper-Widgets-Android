5499266b2118fd3f1f0b3125b6d465d39e66ae8d
Saad <saadahmedscse@gmail.com>
2023-01-28 22:37:47 +0600

    Created Constraint Layout

---
+++ HelperWidget/src/main/java/com/saadahmedev/helperwidget/widgets/ConstraintLayout.java
@@ -0,0 +1,102 @@
+package com.saadahmedev.helperwidget.widgets;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.GradientDrawable;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.saadahmedev.helperwidget.Helper;
+import com.saadahmedev.helperwidget.R;
+import com.saadahmedev.helperwidget.interfaces.OnLongPressListener;
+
+public class ConstraintLayout extends androidx.constraintlayout.widget.ConstraintLayout {
+    public ConstraintLayout(@NonNull Context context) {
+        super(context);
+
+        init(context, null, 0);
+    }
+
+    public ConstraintLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        init(context, attrs, 0);
+    }
+
+    public ConstraintLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        init(context, attrs, defStyleAttr);
+    }
+
+    @NonNull
+    private static int[] cornerRadiusIds() {
+        return new int[]{
+                R.styleable.ConstraintLayout_cornerRadius,
+                R.styleable.ConstraintLayout_topLeftCornerRadius,
+                R.styleable.ConstraintLayout_topRightCornerRadius,
+                R.styleable.ConstraintLayout_bottomRightCornerRadius,
+                R.styleable.ConstraintLayout_bottomLeftCornerRadius,
+        };
+    }
+
+    @NonNull
+    private static int[] colorIds() {
+        return new int[]{
+                R.styleable.ConstraintLayout_backgroundColor,
+                R.styleable.ConstraintLayout_rippleColor
+        };
+    }
+
+    @NonNull
+    private static int[] strokeIds() {
+        return new int[]{
+                R.styleable.ConstraintLayout_strokeWidth,
+                R.styleable.ConstraintLayout_strokeColor
+        };
+    }
+
+    private void init(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.ConstraintLayout, defStyleAttr, 0);
+        Helper.initView(this, context, typedArray, new GradientDrawable());
+
+        Helper.initDefaultColors(colorIds());
+        Helper.initDefaultCornerRadius(cornerRadiusIds());
+        Helper.initDefaultClickable(R.styleable.ConstraintLayout_clickable);
+        Helper.initDefaultShape(R.styleable.ConstraintLayout_shape);
+        Helper.initDefaultStroke(strokeIds());
+
+        Helper.completeView();
+        typedArray.recycle();
+    }
+
+    public void enable() {
+        Helper.enable();
+    }
+
+    public void disable() {
+        Helper.disable();
+    }
+
+    public void visible() {
+        Helper.visible();
+    }
+
+    public void invisible() {
+        Helper.invisible();
+    }
+
+    public void gone() {
+        Helper.gone();
+    }
+
+    public void onClicked(com.saadahmedev.helperwidget.interfaces.OnClickListener listener) {
+        Helper.onClicked(listener);
+    }
+
+    public void onLongPressed(OnLongPressListener listener) {
+        Helper.onLongPressed(listener);
+    }
+}
