888a81ca775436daf42d5639f5026fe141d686db
saada <saadahmedscse@gmail.com>
2023-01-28 17:27:21 +0600

    Optimized code

--- HelperWidget/src/main/java/com/saadahmedev/helperwidget/utils/FontFamily.java
+++ HelperWidget/src/main/java/com/saadahmedev/helperwidget/utils/FontFamily.java
@@ -1,7 +1,17 @@
 package com.saadahmedev.helperwidget.utils;
 
 public enum FontFamily {
-    REGULAR,
-    MEDIUM,
-    BOLD
+    REGULAR("regular.ttf"),
+    MEDIUM("medium.ttf"),
+    BOLD("bold.ttf");
+
+    private final String value;
+
+    FontFamily(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
--- HelperWidget/src/main/java/com/saadahmedev/helperwidget/utils/Shape.java
+++ HelperWidget/src/main/java/com/saadahmedev/helperwidget/utils/Shape.java
@@ -1,17 +1,27 @@
 package com.saadahmedev.helperwidget.utils;
 
 public enum Shape {
-    ROUNDED_5DP,
-    ROUNDED_10DP,
-    ROUNDED_15DP,
-    ROUNDED_20DP,
-    ROUNDED_25DP,
-    ROUNDED_30DP,
-    ROUNDED_35DP,
-    ROUNDED_40DP,
-    ROUNDED_45DP,
-    ROUNDED_50DP,
-    OVAL,
-    RING,
-    NORMAL
+    ROUNDED_5DP(5F),
+    ROUNDED_10DP(10F),
+    ROUNDED_15DP(15F),
+    ROUNDED_20DP(20F),
+    ROUNDED_25DP(25F),
+    ROUNDED_30DP(30F),
+    ROUNDED_35DP(35F),
+    ROUNDED_40DP(40F),
+    ROUNDED_45DP(45F),
+    ROUNDED_50DP(50F),
+    OVAL(0F),
+    RING(0F),
+    NORMAL(0F);
+
+    private final Float value;
+
+    Shape(Float value) {
+        this.value = value;
+    }
+
+    public float getValue() {
+        return value;
+    }
 }
